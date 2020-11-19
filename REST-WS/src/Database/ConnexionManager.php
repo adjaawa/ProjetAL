48b278e06483b053a47f1e1883292bd349be0e5f
adjaawa <45859929+adjaawa@users.noreply.github.com>
2020-11-19 04:44:02 +0000

    Add files via upload

---
+++ REST-WS/src/Database/ConnexionManager.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Connexion à la base de données MYSQL
+ */
+
+class Database
+{
+    private $_db;
+
+    public function __construct()
+    {
+        $host = "localhost";
+        $dbname = "samaye_news";
+        $user = "root";
+        $pass = "root";
+
+        try {
+            $this->_db = new \PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+            $this->_db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\PDOException $e) {
+            exit("Connection to database failed: " .$e->getMessage());
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->_db;
+    }
+}
